91771dfb41a7a8a945532cc65207bb2a9265638a
xeorleon <xeorleon@gmail.com>
2021-01-03 17:27:06 +0300

    last commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use("/", express.static(path.join(__dirname, "/dist")))
 
-const port = process.env || 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port);
 
